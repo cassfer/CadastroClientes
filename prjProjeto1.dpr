547d8e2e2e1cca951e1dfe15dc90606920ca139a
cassfer <cassiohafernandes@gmail.com>
2020-11-10 00:47:57 -0300

    adicionado database e formulario

---
+++ prjProjeto1.dpr
@@ -0,0 +1,14 @@
+program prjProjeto1;
+
+uses
+  Vcl.Forms,
+  uFormMain in 'forms\uFormMain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
